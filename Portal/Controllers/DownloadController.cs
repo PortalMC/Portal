f5d85bfe5eb9c01802023cefd3e3ee48819f65c4
Ralph <git@ralph.ms>
2017-12-07 04:46:38 +0900

    Add download panel

---
+++ Portal/Controllers/DownloadController.cs
@@ -0,0 +1,28 @@
+﻿using System.IO;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Portal.Controllers
+{
+    public class DownloadController : Controller
+    {
+        public IActionResult Index(string version)
+        {
+            if (version != null)
+            {
+                var path = Path.Combine(Directory.GetCurrentDirectory(), "coremods", $"{version}.jar");
+                return PhysicalFile(path, "application/java-archive", $"portal-core-{version}.jar");
+            }
+            return View();
+        }
+
+        public IActionResult Installation()
+        {
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
